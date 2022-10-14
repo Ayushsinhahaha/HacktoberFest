015f0cccd8b4039318d86a6b1ededa100b20cf90
Prakhar Gupta <68066359+prakhar1032@users.noreply.github.com>
2022-10-14 11:53:59 +0530

    Create Majority_Element.cpp

---
+++ Majority_Element.cpp
@@ -0,0 +1,21 @@
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        map<int,int> mp;
+        for(int i=0;i<size;i++){
+            mp[a[i]]++;
+        }
+        for(auto it:mp){
+            if(it.second>(size/2))
+            return(it.first);
+        }
+        return -1;
+        
+    }
+};
